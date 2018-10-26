9989d87e905c9db774edc42644869c957c5de603
Fabio Picheli <fabio.picheli@betsys.com>
2018-10-26 12:19:04 +0200

    feat: improve library

--- projects/counter/src/lib/counter.component.ts
+++ projects/counter/src/lib/counter.component.ts
@@ -1,19 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'lib-counter',
-  template: `
-    <p>
-      counter works!
-    </p>
-  `,
+  templateUrl: './counter.component.html',
   styles: []
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent {
+  counter = 0;
 
-  constructor() { }
-
-  ngOnInit() {
+  increase() {
+    this.counter++;
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CounterModule } from 'counter';
+import { CounterModule } from '../../projects/counter/src/lib/counter.module';
 
 import { AppComponent } from './app.component';
 
